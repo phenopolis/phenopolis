8cdfa2091ef5c5b859096bbe759409a6018667a8
pontikos <n.pontikos@ucl.ac.uk>
2016-10-13 00:43:15 +0100

    mongodb/__init__.py

---
+++ mongodb/__init__.py
@@ -0,0 +1,252 @@
+### all the mongodb reading/writing code lives here now
+
+
+def load_db():
+    """
+    Load the database
+    """
+    # Initialize database
+    # Don't need to explicitly create tables with mongo, just indices
+    confirm = raw_input('This will drop the database and reload. Are you sure you want to continue? [no] ')
+    if not confirm.startswith('y'):
+        print('Exiting...')
+        sys.exit(1)
+    all_procs = []
+    for load_function in [load_variants_file, load_dbsnp_file, load_base_coverage, load_gene_models, load_constraint_information]:
+        procs = load_function()
+        all_procs.extend(procs)
+        print("Started %s processes to run %s" % (len(procs), load_function.__name__))
+
+    [p.join() for p in all_procs]
+    print('Done! Loading MNPs...')
+    load_mnps()
+    print('Done! Creating cache...')
+    create_cache()
+    print('Done!')
+
+
+def load_base_coverage():
+    """ """
+    def load_coverage(coverage_files, i, n, db):
+        coverage_generator = parse_tabix_file_subset(coverage_files, i, n, get_base_coverage_from_file)
+        try:
+            db.base_coverage.insert(coverage_generator, w=0)
+        except pymongo.errors.InvalidOperation, e:
+            print(e)
+            # handle error when coverage_generator is empty
+            pass  
+    db = get_db()
+    db.base_coverage.drop()
+    print("Dropped db.base_coverage")
+    # load coverage first; variant info will depend on coverage
+    db.base_coverage.ensure_index('xpos')
+    procs = []
+    coverage_files = app.config['BASE_COVERAGE_FILES']
+    num_procs = app.config['LOAD_DB_PARALLEL_PROCESSES']
+    random.shuffle(app.config['BASE_COVERAGE_FILES'])
+    for i in range(num_procs):
+        p = Process(target=load_coverage, args=(coverage_files, i, num_procs, db))
+        p.start()
+        procs.append(p)
+    return procs
+    #print 'Done loading coverage. Took %s seconds' % int(time.time() - start_time)
+
+
+def load_variants_file():
+    def load_variants(sites_file, i, n, db):
+        for f in sites_file:
+            variants_generator = parse_tabix_file_subset([f], i, n, get_variants_from_sites_vcf)
+            try:
+                db.variants.insert(variants_generator, w=0)
+            except pymongo.errors.InvalidOperation:
+                pass  # handle error when variant_generator is empty
+    db = get_db()
+    db.variants.drop()
+    print("Dropped db.variants")
+    # grab variants from sites VCF
+    db.variants.ensure_index('xpos')
+    db.variants.ensure_index('xstart')
+    db.variants.ensure_index('xstop')
+    db.variants.ensure_index('rsid')
+    db.variants.ensure_index('genes')
+    db.variants.ensure_index('transcripts')
+    sites_vcfs = app.config['SITES_VCFS']
+    print(sites_vcfs)
+    #if len(sites_vcfs) > 1: raise Exception("More than one sites vcf file found: %s" % sites_vcfs)
+    procs = []
+    num_procs = app.config['LOAD_DB_PARALLEL_PROCESSES']
+    #pdb.set_trace()
+    for i in range(num_procs):
+        p = Process(target=load_variants, args=(sites_vcfs, i, num_procs, db))
+        p.start()
+        procs.append(p)
+    return procs
+
+    #print 'Done loading variants. Took %s seconds' % int(time.time() - start_time)
+
+
+def load_constraint_information():
+    db = get_db()
+
+    db.constraint.drop()
+    print 'Dropped db.constraint.'
+
+    start_time = time.time()
+
+    with gzip.open(app.config['CONSTRAINT_FILE']) as constraint_file:
+        for transcript in get_constraint_information(constraint_file):
+            db.constraint.insert(transcript, w=0)
+
+    db.constraint.ensure_index('transcript')
+    print 'Done loading constraint info. Took %s seconds' % int(time.time() - start_time)
+
+
+def load_mnps():
+    db = get_db()
+    start_time = time.time()
+
+    db.variants.ensure_index('has_mnp')
+    print 'Done indexing.'
+    while db.variants.find_and_modify({'has_mnp' : True}, {'$unset': {'has_mnp': '', 'mnps': ''}}): pass
+    print 'Deleted MNP data.'
+
+    with gzip.open(app.config['MNP_FILE']) as mnp_file:
+        for mnp in get_mnp_data(mnp_file):
+            variant = lookups.get_raw_variant(db, mnp['xpos'], mnp['ref'], mnp['alt'], True)
+            db.variants.find_and_modify({'_id': variant['_id']}, {'$set': {'has_mnp': True}, '$push': {'mnps': mnp}}, w=0)
+
+    db.variants.ensure_index('has_mnp')
+    print 'Done loading MNP info. Took %s seconds' % int(time.time() - start_time)
+
+
+def load_gene_models():
+    db = get_db()
+
+    db.genes.drop()
+    db.transcripts.drop()
+    db.exons.drop()
+    print 'Dropped db.genes, db.transcripts, and db.exons.'
+
+    start_time = time.time()
+
+    canonical_transcripts = {}
+    with gzip.open(app.config['CANONICAL_TRANSCRIPT_FILE']) as canonical_transcript_file:
+        for gene, transcript in get_canonical_transcripts(canonical_transcript_file):
+            canonical_transcripts[gene] = transcript
+
+    omim_annotations = {}
+    with gzip.open(app.config['OMIM_FILE']) as omim_file:
+        for fields in get_omim_associations(omim_file):
+            if fields is None:
+                continue
+            gene, transcript, accession, description = fields
+            omim_annotations[gene] = (accession, description)
+
+    dbnsfp_info = {}
+    with gzip.open(app.config['DBNSFP_FILE']) as dbnsfp_file:
+        for dbnsfp_gene in get_dbnsfp_info(dbnsfp_file):
+            other_names = [other_name.upper() for other_name in dbnsfp_gene['gene_other_names']]
+            dbnsfp_info[dbnsfp_gene['ensembl_gene']] = (dbnsfp_gene['gene_full_name'], other_names)
+
+    print 'Done loading metadata. Took %s seconds' % int(time.time() - start_time)
+
+    # grab genes from GTF
+    start_time = time.time()
+    with gzip.open(app.config['GENCODE_GTF']) as gtf_file:
+        for gene in get_genes_from_gencode_gtf(gtf_file):
+            gene_id = gene['gene_id']
+            if gene_id in canonical_transcripts:
+                gene['canonical_transcript'] = canonical_transcripts[gene_id]
+            if gene_id in omim_annotations:
+                gene['omim_accession'] = omim_annotations[gene_id][0]
+                gene['omim_description'] = omim_annotations[gene_id][1]
+            if gene_id in dbnsfp_info:
+                gene['full_gene_name'] = dbnsfp_info[gene_id][0]
+                gene['other_names'] = dbnsfp_info[gene_id][1]
+            db.genes.insert(gene, w=0)
+
+    print 'Done loading genes. Took %s seconds' % int(time.time() - start_time)
+
+    start_time = time.time()
+    db.genes.ensure_index('gene_id')
+    db.genes.ensure_index('gene_name_upper')
+    db.genes.ensure_index('gene_name')
+    db.genes.ensure_index('other_names')
+    db.genes.ensure_index('xstart')
+    db.genes.ensure_index('xstop')
+    print 'Done indexing gene table. Took %s seconds' % int(time.time() - start_time)
+
+    # and now transcripts
+    start_time = time.time()
+    with gzip.open(app.config['GENCODE_GTF']) as gtf_file:
+        db.transcripts.insert((transcript for transcript in get_transcripts_from_gencode_gtf(gtf_file)), w=0)
+    print 'Done loading transcripts. Took %s seconds' % int(time.time() - start_time)
+
+    start_time = time.time()
+    db.transcripts.ensure_index('transcript_id')
+    db.transcripts.ensure_index('gene_id')
+    print 'Done indexing transcript table. Took %s seconds' % int(time.time() - start_time)
+
+    # Building up gene definitions
+    start_time = time.time()
+    with gzip.open(app.config['GENCODE_GTF']) as gtf_file:
+        db.exons.insert((exon for exon in get_exons_from_gencode_gtf(gtf_file)), w=0)
+    print 'Done loading exons. Took %s seconds' % int(time.time() - start_time)
+
+    start_time = time.time()
+    db.exons.ensure_index('exon_id')
+    db.exons.ensure_index('transcript_id')
+    db.exons.ensure_index('gene_id')
+    print 'Done indexing exon table. Took %s seconds' % int(time.time() - start_time)
+
+    return []
+
+
+def load_dbsnp_file():
+    db = get_db()
+    def load_dbsnp(dbsnp_file, i, n, db):
+        if os.path.isfile(dbsnp_file + ".tbi"):
+            dbsnp_record_generator = parse_tabix_file_subset([dbsnp_file], i, n, get_snp_from_dbsnp_file)
+            try:
+                db.dbsnp.insert(dbsnp_record_generator, w=0)
+            except pymongo.errors.InvalidOperation:
+                pass  # handle error when coverage_generator is empty
+
+        else:
+            with gzip.open(dbsnp_file) as f:
+                db.dbsnp.insert((snp for snp in get_snp_from_dbsnp_file(f)), w=0)
+    db.dbsnp.drop()
+    db.dbsnp.ensure_index('rsid')
+    db.dbsnp.ensure_index('xpos')
+    start_time = time.time()
+    dbsnp_file = app.config['DBSNP_FILE']
+
+    print "Loading dbsnp from %s" % dbsnp_file
+    if os.path.isfile(dbsnp_file + ".tbi"):
+        num_procs = app.config['LOAD_DB_PARALLEL_PROCESSES']
+    else:
+        # see if non-tabixed .gz version exists
+        if os.path.isfile(dbsnp_file):
+            print(("WARNING: %(dbsnp_file)s.tbi index file not found. Will use single thread to load dbsnp."
+                "To create a tabix-indexed dbsnp file based on UCSC dbsnp, do: \n"
+                "   wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/snp141.txt.gz \n"
+                "   gzcat snp141.txt.gz | cut -f 1-5 | bgzip -c > snp141.txt.bgz \n"
+                "   tabix -0 -s 2 -b 3 -e 4 snp141.txt.bgz") % locals())
+            num_procs = 1
+        else:
+            raise Exception("dbsnp file %s(dbsnp_file)s not found." % locals())
+
+    procs = []
+    for i in range(num_procs):
+        p = Process(target=load_dbsnp, args=(dbsnp_file, i, num_procs, db))
+        p.start()
+        procs.append(p)
+
+    return procs
+    #print 'Done loading dbSNP. Took %s seconds' % int(time.time() - start_time)
+    #start_time = time.time()
+    #db.dbsnp.ensure_index('rsid')
+    #print 'Done indexing dbSNP table. Took %s seconds' % int(time.time() - start_time)
+
+
+
