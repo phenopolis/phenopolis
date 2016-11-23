4abad4cd01d71d59462a64ab968e836950662b91
pontikos <n.pontikos@ucl.ac.uk>
2016-11-23 20:53:43 +0000

    __init__.py

--- views/__init__.py
+++ views/__init__.py
@@ -116,6 +116,7 @@ def check_auth(username, password):
     Will try to connect to phenotips instance.
     """
     print username
+    #return True
     conn=PhenotipsClient()
     response=conn.get_patient(auth='%s:%s' % (username, password,),number=1)
     if response:
