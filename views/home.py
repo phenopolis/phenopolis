a8ad94414dd48b75827e71370883e1548d052e6a
pontikos <n.pontikos@ucl.ac.uk>
2017-02-20 11:13:40 +0000

    added template variables to home page

--- views/home.py
+++ views/home.py
@@ -17,4 +17,56 @@ import subprocess
 @app.route('/', methods=['GET'])
 def homepage():
     cache_key = 't-home'
-    return render_template('home.html', title='Phenopolis - Home Page')
+    db=get_db()
+    patients_db=get_db(app.config['DB_NAME_PATIENTS']) 
+    total_variants=db.variants.count()
+    total_variants=db.variants.count()
+    print('total_variants',total_variants,)
+    total_patients=patients_db.patients.count()
+    print('total_patients',total_patients,)
+    male_patients=patients_db.patients.find( {'sex':'M'}).count()
+    print('male_patients',male_patients,)
+    female_patients=patients_db.patients.find( {'sex':'F'}).count()
+    print('female_patients',female_patients,)
+    unknown_patients=patients_db.patients.find( {'sex':'U'}).count()
+    if config.LOCAL:
+        hpo_json={}
+    else:
+        hpo_file='uclex_stats/overall_hpo_2016_Aug_2.json'
+        hpo_json = json.load(open(hpo_file,'r'))
+    exac_variants=0
+    print('exac_variants',exac_variants,)
+    pass_variants=db.variants.find({'FILTER':'PASS'}).count()
+    print('pass_variants',pass_variants,)
+    #pass_exac_variants=db.variants.find({'in_exac':True,'filter':'PASS'}).count()
+    #pass_exac_variants=db.variants.find({'in_exac':True,'filter':'PASS'}).count()
+    pass_exac_variants=0
+    print('pass_exac_variants',pass_exac_variants,)
+    #pass_exac_variants=db.variants.find({'in_exac':True,'filter':'PASS'}).count()
+    pass_exac_variants=0
+    #nonexac_variants=db.variants.find({'in_exac':False}).count()
+    nonexac_variants=0
+    #pass_nonexac_variants=db.variants.find({'in_exac':False,'filter':'PASS'}).count()
+    pass_nonexac_variants=0
+    nonpass_variants=(total_variants-pass_variants)
+    nonpass_nonexac_variants=nonexac_variants-pass_nonexac_variants
+    try:
+        version_number = subprocess.check_output(['git', 'describe', '--exact-match'])
+    except:
+        version_number = None
+    print('Version number is:-')
+    print(version_number)
+    return render_template('home.html', title='Phenopolis - Home Page',
+        total_patients=total_patients,
+        male_patients=male_patients,
+        female_patients=female_patients,
+        unknown_patients=unknown_patients,
+        hpo_json=json.dumps(hpo_json),
+        total_variants=total_variants,
+        exac_variants=exac_variants,
+        pass_variants=pass_variants,
+        nonpass_variants=nonpass_variants,
+        pass_exac_variants=pass_exac_variants,
+        pass_nonexac_variants=pass_nonexac_variants,
+        version_number=version_number)
+
