cdf759c7a1a1666b322f9c667d3f5ad090535cf0
Nikolas Pontikos <npontikos@gmail.com>
2017-03-10 17:25:42 +0000

    added users to easy_install.sh and tests/data

--- easy_install.sh
+++ easy_install.sh
@@ -77,6 +77,11 @@ mongo patients --eval "db.patients.createIndex({'solved' : 1})"
 mongo patients --eval "db.patients.createIndex({'clinicalStatus.clinicalStatus' : 1})"
 mongo patients --eval "db.patients.createIndex({'specificity.score' : 1})"
 
+mongoimport --db users --collection users --file tests/data/users.json --drop
+mongo users --eval "db.genes_pheno.createIndex({'gene' : 1})"
+mongo users --eval "db.genes_pheno.createIndex({'hpo' : 1})"
+
+
 # Run server
 cd phenopolis 
 python runserver.py
