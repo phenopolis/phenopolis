0e2e6b45950c9dc9acea904e152df9b778f53824
IsmailM <ismail.moghul@gmail.com>
2017-03-20 01:26:20 +0000

    update the individual js

--- static/assets/js/individual.js
+++ static/assets/js/individual.js
@@ -22,10 +22,10 @@ if (!PP) {
   };
 
   // init the table directly under the header
-  PP.initPatientFeatureTable = function (patient_id) {
+  PP.initPatientFeatureTable = function(patient_id) {
     $.ajax({
       type: 'GET',
-      url: 'individual_json/' + patient_id, // TODO - Parse error
+      url: '/individual_json/' + patient_id,
       dataType: 'json',
       timeout: 120000,
       success: function(data) { PP.PatientFeatureSuccess(data); },
@@ -35,11 +35,17 @@ if (!PP) {
 
   //
   PP.PatientFeatureSuccess = function(data) {
-    PP.addPatientFeaturesInfo(data.result.features); // Or observed_features
+    PP.addPatientGenderInfo(data.result.gender); // Or observed_features
+    PP.addPatientFeaturesInfo(data.result.observed_features); // Or observed_features
     PP.addPatientConsanguinityInfo(data.result.family_history);
-    PP.addPatientInheritanceModeInfo(data.reult); //todo
     PP.addPatientGenesInfo(data.result.genes);
-    $('#info_table').show();
+    $('.info_table').show();
+  };
+
+
+  PP.addPatientGenderInfo = function(gender) {
+    var gender_full = gender === 'M' ? 'Male' : 'Female';
+    $('#patient_gender').text(gender_full);
   };
 
   //
@@ -53,14 +59,15 @@ if (!PP) {
 
   //
   PP.addPatientConsanguinityInfo = function(family_history) {
-    if (family_history === undefined ) return;
-    if (family_history.consanguinity === undefined || family_history.consanguinity === null ) return;
+    if (family_history !== undefined) {
+      $('#patient_consanguinity').closest('tr').hide();
+      return;
+    }
+    if (family_history.consanguinity === undefined || family_history.consanguinity === null) {
+      $('#patient_consanguinity').closest('tr').hide();
+      return;
+    }
     $('#patient_consanguinity').text(family_history.consanguinity);
-  };
-
-  //
-  PP.addPatientInheritanceModeInfo = function() {
-    $('#patient_inheritance_mode').text('TODO');
   };
 
   //
@@ -73,69 +80,174 @@ if (!PP) {
   };
 
 
-  PP.initOmimPlot = function () {
+  PP.initOmimPlot = function(patient_id) {
+    $.ajax({
+      type: 'GET',
+      url: '/venn_json/' + patient_id,
+      dataType: 'json',
+      timeout: 120000,
+      success: function(data) { PP.vennSuccess(data); },
+      error: function(data, msg) { PP.ajaxError(data, msg); }
+    });
+  };
 
+  PP.vennSuccess = function(data) {
+    sets = PP.generateSets(data.result);
+    PP.createVennDiagram(sets);
+    $('#omim_tab #progress_row').remove();
+  };
+
+  PP.generateSets = function(data) {
+    sets = [];
+    for (var i = 0; i < data.length; i++) {
+      sets.push({ "sets": data[i].key, "size": data[i].value.length });
+    }
+    return sets;
+  };
+
+  PP.createVennDiagram = function(sets) {
+    // draw venn diagram
+    var div = d3.select("#venn");
+    div.datum(sets).call(venn.VennDiagram().width(500).height(500));
+
+    div.selectAll("path")
+        .style("stroke-opacity", 0)
+        .style("stroke", "#fff")
+        .style("stroke-width", 3);
+
+    // add a tooltip
+    var tooltip = d3.select("body").append("div")
+      .attr("class", "venntooltip");
+
+    // add listeners to all the groups to display tooltip on mouseover
+    div.selectAll("g")
+      .on("mouseover", function(d, i) {
+        // sort all the areas relative to the current item
+        venn.sortAreas(div, d);
+
+        // Display a tooltip with the current size
+        tooltip.transition().duration(400).style("opacity", 0.9);
+        tooltip.text(d.size + " Genes");
+
+        // highlight the current path
+        var selection = d3.select(this).transition("tooltip").duration(400);
+        selection.select("path")
+          .style("stroke-width", 3)
+          .style("fill-opacity", d.sets.length == 1 ? 0.5 : 0.1)
+          .style("stroke-opacity", 1);
+      })
+      .on("mousemove", function() {
+        tooltip.style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d, i) {
+        tooltip.transition().duration(400).style("opacity", 0);
+        var selection = d3.select(this).transition("tooltip").duration(400);
+        selection.select("path")
+          .style("stroke-width", 0)
+          .style("fill-opacity", d.sets.length == 1 ? 0.25 : 0.0)
+          .style("stroke-opacity", 0);
+      });
   };
 
   PP.initHomsTable = function(patient_id) {
     $.ajax({
       type: 'GET',
-      url: 'homozygous_variants_json/' + patient_id,
+      url: '/homozygous_variants_json/' + patient_id,
       dataType: 'json',
       timeout: 120000,
-      success: function(data) { PP.HomsSuccess(data); },
+      success: function(data) { PP.variantTableSuccess(data); },
       error: function(data, msg) { PP.ajaxError(data, msg); }
     });
   };
 
-  PP.HomsSuccess = function(data) {
-    var homs = data; // TODO
-    for (var i = 0; i < homs.length; i++) {
+  PP.initCompHetsTable = function(patient_id) {
+    // $.ajax({
+    //   type: 'GET',
+    //   url: '/compound_het_variants_json/' + patient_id,
+    //   dataType: 'json',
+    //   timeout: 120000,
+    //   success: function(data) { PP.variantTableSuccess(data); },
+    //   error: function(data, msg) { PP.ajaxError(data, msg); }
+    // });
+  };
+
+  PP.initVariantsTable = function(patient_id) {
+    // $.ajax({
+    //   type: 'GET',
+    //   url: '/rare_variants_json/' + patient_id,
+    //   dataType: 'json',
+    //   timeout: 120000,
+    //   success: function(data) { PP.variantTableSuccess(data); },
+    //   error: function(data, msg) { PP.ajaxError(data, msg); }
+    // });
+  };
+
+  PP.variantTableSuccess = function(data) {
+    var d = data.result;
+    for (var i = 0; i < d.length; i++) {
       // generate tr
       $('<tr>').append(
-         $('<td>').html(),
-         $('<td>').text()
+        $('<td>').html(PP.generateGeneNames(d[i].transcript_consequences)),
+        $('<td>').text(' '),
+        $('<td>').text(' '),
+        $('<td>').html(PP.generateHpoLinks(d[i].HPO)),
+        $('<td>').text(' '),
+        $('<td>').html(PP.generateVariantHtml(d[i].synonym_variant_id)),
+        $('<td>').text(' '),
+        $('<td>').text(' '),
+        $('<td>').text(' '),
+        $('<td>').text(d[i].most_severe_consequence),
+        $('<td>').html(PP.createHgvspTd(d[i].canonical_hgvsp, d[i].canonical_hgvsc)),
+        $('<td>').text(' '),
+        $('<td>').text(' '),
+        $('<td>').html(PP.generateIndividualHtml(d[i].hom_samples)),
+        $('<td>').text()
       ).appendTo('#homs_table_body');
     }
-    $('#homs #progress_row').remove();
+    $('#homs_tab #progress_row').remove();
     $('#homs_table').show();
     PP.initTableSorter('#homs_table');
   };
 
-  PP.initCompHetsTable = function(patient_id) {
-    $.ajax({
-      type: 'GET',
-      url: 'compound_het_variants_json/' + patient_id,
-      dataType: 'json',
-      timeout: 120000,
-      success: function(data) { PP.CompHetSuccess(data); },
-      error: function(data, msg) { PP.ajaxError(data, msg); }
-    });
+  PP.generateGeneNames = function(transcript_consequences) {
+    geneNames = [];
+    for (var i = 0; i < transcript_consequences.length; i++) {
+      url = PP.create_url('/gene', transcript_consequences[i].gene_symbol);
+      if (geneNames.indexOf(url) === -1) {
+        geneNames.push(url);
+      }
+    }
+    return (geneNames.join('<br>'));
   };
 
-  PP.CompHetSuccess = function(data) {
-
+  PP.generateHpoLinks = function(hpoTerms) {
+    hpoHtml = '';
+    for (var i = 0; i < hpoTerms.length; i++) {
+      hpoHtml = hpoHtml + PP.create_url('/hpo', hpoTerms[i].hpo_term, hpoTerms[i].hpo_id, 'chip');
+    }
+    return (hpoHtml);
   };
 
-  PP.initVariantsTable = function(patient_id) {
-    $.ajax({
-      type: 'GET',
-      url: 'rare_variants_json/' + patient_id,
-      dataType: 'json',
-      timeout: 120000,
-      success: function(data) { PP.VariantsSuccess(data); },
-      error: function(data, msg) { PP.ajaxError(data, msg); }
-    });
+  PP.generateVariantHtml = function(variant) {
+    return PP.create_url('/variant', variant) + '<br><a class="text-small" href="/sequence?variant_id=' + variant + '&symbol=&build=grch37">Primer Design</a>';
   };
 
-  PP.VariantsSuccess = function(data) {
-
+  PP.generateIndividualHtml = function(individuals) {
+    return individuals.join(' ');
   };
+
+  PP.createHgvspTd = function(hgvsp, hgvsc) {
+    if (hgvsp === undefined) return;
+    if (hgvsc === undefined) return;
+    return '<span data="' + hgvsc[0] + '">' + hgvsp[0].split(':')[1] + '</span>';
+  };
+
 
   PP.initExomiserTab = function(patient_id) {
     // $.ajax({
     //   type: 'GET',
-    //   url: 'homozygous_variants_json/',
+    //   url: '/homozygous_variants_json/',
     //   dataType: 'json',
     //   timeout: 120000,
     //   success: function(data) { PP.HomsSuccess(data); },
