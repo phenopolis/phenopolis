6ae36cc706397c89ea3d92a0f97028cbdbf83db9
IsmailM <ismail.moghul@gmail.com>
2017-03-21 19:20:58 +0000

    add variant page

---
+++ static/assets/js/variant.js
@@ -0,0 +1,18 @@
+/*
+    PP - Phenopolis's JavaScript module
+
+    Define a global PP (acronym for Phenopolis) object containing all
+    PP associated methods:
+*/
+
+// define global PP object
+var PP;
+if (!PP) {
+  PP = {};
+}
+
+// PP module
+(function() {
+
+}());
+// End of PP module
