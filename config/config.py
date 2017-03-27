86f6f2839c908e0a6cc0903212695b1bfbd6ab0f
Withington <lucy.withington@outlook.com>
2017-03-27 17:40:46 +0100

    Add authentication using argon2 algorithm (inactive by default, use config.USE_ARGON2_AUTH to test it).

--- config/config.py
+++ config/config.py
@@ -7,4 +7,4 @@ else:
 
 IMPORT_PYSAM_PRIMER3 = True # Set this to False to develop on Windows, where pysam and primer3 fail to install.
 
-USE_ARGON2_AUTH = False # To test, populate USER_DB by entering  >>mongoimport --db users --collection users --file tests/data/users_argon2.json --drop
+USE_ARGON2_AUTH = False # To test, set to True and populate USER_DB by entering  >>mongoimport --db users --collection users --file tests/data/users_argon2.json --drop
