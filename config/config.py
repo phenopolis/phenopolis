5b45fb3a14879fb0eed26f51e37ff2be5c599f50
Withington <lucy.withington@outlook.com>
2017-10-09 09:19:28 +0100

    Corrected config setting.

--- config/config.py
+++ config/config.py
@@ -5,5 +5,5 @@ if len(sys.argv)>1 and sys.argv[1]=='SERVER':
 else:
     LOCAL=True
 
-IMPORT_PYSAM_PRIMER3 = False # Set this to False to develop on Windows, where pysam and primer3 fail to install.
+IMPORT_PYSAM_PRIMER3 = True # Set this to False to develop on Windows, where pysam and primer3 fail to install.
 
