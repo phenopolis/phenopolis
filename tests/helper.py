f0650bd71357636d17a984c4156c313ade947c73
Withington <lucy.withington@outlook.com>
2017-11-22 16:20:45 +0000

    Added more test data. Added test on page venn_json/person2.

--- tests/helper.py
+++ tests/helper.py
@@ -69,7 +69,7 @@ def my_patients_neo4j_data():
         s="""
         MATCH (u:User {user:'%s'})
         MERGE (u)-[r:WRITES]->(p:Person {personId:"person1", gender:"M", score:0.69})
-        MERGE (t:Term {termId:"HP:0000505", name:"Visual impairment"})
+        MERGE (t:Term {termId:"HP:0000505", name:"Visual impairment", observed:"yes"})
         MERGE (p)-[:PersonToObservedTerm]->(t)
         MERGE (p)-[:CandidateGene]->(g:Gene {gene_name:"TTLL5"})
         MERGE (gv1:GeneticVariant {variantId:"22-38212762-A-G", allele_freq:0.0002, kaviar_AF:0.000006})
@@ -85,13 +85,16 @@ def my_patients_neo4j_data():
             (gv1:GeneticVariant {variantId:"22-38212762-A-G"}), 
             (gv2:GeneticVariant {variantId:"14-76201609-C-G"})
         MERGE (u)-[r:WRITES]->(p:Person {personId:"person2", gender:"F", score:0.69})
-        MERGE (t505:Term {termId:"HP:0000505", name:"Visual impairment"})
-        MERGE (t479:Term {termId:"HP:0000479", name:"Abnormality of the retina"})
+        MERGE (t505:Term {termId:"HP:0000505", name:"Visual impairment", observed:"yes"})
+        MERGE (t479:Term {termId:"HP:0000479", name:"Abnormality of the retina", observed:"yes"})
+        MERGE (t7754:Term {termId:"HP:0007754", name:"Macular dystrophy", observed:"yes"})
         MERGE (p)-[:PersonToObservedTerm]->(t505)
         MERGE (p)-[:PersonToObservedTerm]->(t479)
+        MERGE (p)-[:PersonToObservedTerm]->(t7754)
         MERGE (p)-[:CandidateGene]->(g)
         MERGE (p)-[:CandidateGene]->(g1:Gene {gene_name:"DRAM2"})
         MERGE (p)-[:CandidateGene]->(g2:Gene {gene_name:"RPGR"})
+        MERGE (p)-[:CandidateGene]->(g3:Gene {gene_name:"TRIM32"})
         MERGE (gv3:GeneticVariant {variantId:"14-12312312-C-G", allele_freq:0.0002, kaviar_AF:0.000006})
         MERGE (p)<-[:HomVariantToPerson]-(gv1)
         MERGE (p)<-[:HetVariantToPerson]-(gv2)
@@ -101,10 +104,15 @@ def my_patients_neo4j_data():
 
         #Gene to HPO term
         s="""
-        MATCH (g:Gene {gene_name:"TTLL5"}), 
-            (t505:Term {termId:"HP:0000505"}), (t479:Term {termId:"HP:0000479"})
-        MERGE (g)-[:GeneToTerm]->(t505)
-        MERGE (g)-[:GeneToTerm]->(t479);
+        MATCH (g1:Gene {gene_name:"TTLL5"}), (g2:Gene {gene_name:"DRAM2"}),
+            (g3:Gene {gene_name:"TRIM32"}), 
+            (t505:Term {termId:"HP:0000505"}), (t479:Term {termId:"HP:0000479"}),
+            (t7754:Term {termId:"HP:0007754", name:"Macular dystrophy"})
+        MERGE (g1)-[:GeneToTerm]->(t505)
+        MERGE (g1)-[:GeneToTerm]->(t479)
+        MERGE (g2)-[:GeneToTerm]->(t479)
+        MERGE (g3)-[:GeneToTerm]->(t479)
+        MERGE (g3)-[:GeneToTerm]->(t7754);
         """ 
         result = neo4j_session.run(s)
 
